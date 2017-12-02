860767833cae4581085a876b31ff3d6e6ae77ecc
moontoon <moon.shresth@gmail.com>
2017-12-02 17:56:20 +0545

    to prove a-b ^ 2 formula

---
+++ square.py
@@ -0,0 +1,12 @@
+# to prove (a-b)^2 = a^2 - 2ab + b^2
+
+def lhs(a, b):
+    return(a-b)**2
+
+def rhs(a,b):
+    return a**2 - 2*a*b + b**2
+
+ls=lhs(3,2)
+rs=rhs(3,2)
+
+print("LHS = {}, RHS = {}".format(ls, rs))
