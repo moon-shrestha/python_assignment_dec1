562fc4d0fd19ba816a6926fed910a62c087ede28
moontoon <moon.shresth@gmail.com>
2017-12-02 17:48:44 +0545

    Area of circle

---
+++ cirlcearea.py
@@ -0,0 +1,11 @@
+''' 2. Python program that  accepts the value of radius from user and calculates
+the area of circle '''
+
+PI = 22/7
+
+radius = float(input("Please enter the value of radius:"))
+
+area = PI * radius * radius
+
+print ("The area of circle is: %.3f" %area)
+
